58ef4d3abe312f4fcc09e3d30d7e6e35c9e74bb7
Bpost129 <Bpost129@yahoo.com>
2026-01-15 14:19:31 -0500

    add landing and burger shop routes to navbar, render navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 
 // Components
+import Nav from './Components/Nav/Nav'
 import Landing from './Components/Landing/Landing'
+import BurgerShop from './Components/BurgerShop/BurgerShop'
 
 const App = () => {
   return (
     <main>
-      <Landing />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/burgers' element={<BurgerShop />} />
+      </Routes>
     </main>
   )
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,17 @@
+import { NavLink } from 'react-router-dom'
+
 // Components & Assets
 import Logo from '../../assets/react-logo.png'
 
 const Nav = () => {
   return (
     <nav className="navigation-bar">
-
+      <NavLink to="/" id='logo'>
+        <img src={Logo} alt="react logo" />
+      </NavLink>
+      <NavLink to="/burgers">
+        BURGER SHOP
+      </NavLink>
     </nav>
   )
 }
