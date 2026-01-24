53cc0eb628f5066558e3d121191a00f6ad101c48
Bpost129 <Bpost129@yahoo.com>
2026-01-23 23:19:17 -0500

    update selected po box in state

--- src/Components/PostOffice/NewLetter/SelectBox.jsx
+++ src/Components/PostOffice/NewLetter/SelectBox.jsx
@@ -1,10 +1,10 @@
 
 const SelectBox = ({ setBoxNum, boxNumbers }) => {
   return (
-    <select required name="boxNo">
+    <select required name="boxNo" onChange={(e) => setBoxNum(e.target.value)}>
       <option value="">Select PO Box</option>
       {boxNumbers.map(num =>
-        <option value={num}>PO Box {num}</option>
+        <option key={num} value={num}>PO Box {num}</option>
       )}
     </select>
   )
