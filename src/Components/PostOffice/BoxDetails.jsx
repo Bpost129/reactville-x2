bf04c3ecd19e284982e31961d99a77df01a5a4c0
Bpost129 <Bpost129@yahoo.com>
2026-01-25 23:16:02 -0500

    render message and back link when navigating to non existant box

--- src/Components/PostOffice/BoxDetails.jsx
+++ src/Components/PostOffice/BoxDetails.jsx
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 import LetterList from './LetterList'
 
 const BoxDetails = ({ boxes, letters, markAsRead }) => {
   const { boxNo } = useParams()
   const box = boxes[boxNo]
+  const navigate = useNavigate()
+
+  if (!box) return (
+    <div>
+      <h4>Hmm, this box doesn't seem to exist.</h4>
+      <button onClick={() => navigate('/postoffice')}>Return To Lobby</button>
+    </div>
+  )
 
   return (
     <section className="box-details">
